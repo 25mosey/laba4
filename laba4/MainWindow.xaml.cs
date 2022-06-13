eb812eaa1fa233310fccce3a9da585b0dde01971
никита матросов <nikm100804@gmail.com>
2022-06-13 21:42:18 +0200

    Добавьте файлы проекта.

---
+++ laba4/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace laba4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Solve_Click(object sender, RoutedEventArgs e)
+        {
+            Pair pair = new Pair(double.Parse(First.Text), double.Parse(Second.Text));
+            Result.Text = pair.summ().ToString();
+        }
+
+        private void SolveDiff_Click(object sender, RoutedEventArgs e)
+        {
+            Pair pair = new Pair(double.Parse(First.Text), double.Parse(Second.Text));
+            Result.Text = pair.Diff().ToString();
+        }
+
+        private void SolveDivis_Click(object sender, RoutedEventArgs e)
+        {
+            Pair pair = new Pair(double.Parse(First.Text), double.Parse(Second.Text));
+            Result.Text = pair.Divis().ToString();
+        }
+
+        private void SolveMulti_Click(object sender, RoutedEventArgs e)
+        {
+            Pair pair = new Pair(double.Parse(First.Text), double.Parse(Second.Text));
+            Result.Text = pair.Multi().ToString();
+        }
+    }
+}
---
+++ laba4/Pair.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba4
+{
+    partial class Pair
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public Pair(double a, double b)
+        {
+            A = a;
+            B = b;
+        }
+        public double summ()
+        {
+            return A + B;
+        }
+        public double Diff()
+        {
+            return A - B;
+        }
+        public double Multi()
+        {
+            return A * B;
+        }
+        public double Divis()
+        {
+            return A / B;
+        }
+    }
+}
